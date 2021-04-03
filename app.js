a3f34d6bcf310102c81d0d01e88763ee9ee11c8d
Ashvin Motye <22303668+ashvinmotye@users.noreply.github.com>
2021-04-03 12:18:06 +0400

    03 April 2021 - Add list

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-document.addEventListener("DOMContentLoaded", function(){
-  const target = document.querySelector("#list");
+document.addEventListener("DOMContentLoaded", function () {
+  const moviesList = document.querySelector("#list");
+  const removedList = document.querySelector("#removed");
+
+  const added = getDiff(movies, previous);
+  const removed = getDiff(previous, movies);
 
   movies.forEach(movie => {
-    target.innerHTML += `<li>${movie}</li>`
+    moviesList.innerHTML += `<li>${movie}${added.indexOf(movie) !== -1 ? ' <small><i>(new)</i></small>' : ''}</li>`
+  })
+
+  removed.forEach(movie => {
+    removedList.innerHTML += `<li>${movie}</li>`
   })
 });
+
+const getDiff = (source, target) => {
+  return source.filter(movie => target.indexOf(movie) === -1)
+}
